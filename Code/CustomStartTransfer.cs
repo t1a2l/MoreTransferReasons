1ac8fb8aca158f193032d0252264c0393352581d
Tal <talrofeh@gmail.com>
2023-03-10 21:36:44 +0200

    update to my own transfer manager

---
+++ Code/CustomStartTransfer.cs
@@ -0,0 +1,106 @@
+﻿using ColossalFramework;
+using ColossalFramework.Math;
+using UnityEngine;
+
+namespace MoreTransferReasons.Code
+{
+	public static class CustomStartTransfer
+	{
+		public static void BuildingAIStartTransfer(ushort buildingID, ref Building data, ExtendedTransferManager.TransferReason material, ExtendedTransferManager.Offer offer)
+		{
+			VehicleInfo vehicleInfo = GetSelectedVehicle(buildingID);
+			if (vehicleInfo == null)
+			{
+				vehicleInfo = Singleton<VehicleManager>.instance.GetRandomVehicleInfo(ref Singleton<SimulationManager>.instance.m_randomizer, data.Info.m_class.m_service, data.Info.m_class.m_subService, data.Info.m_class.m_level, VehicleInfo.VehicleType.Car);
+			}
+			if (vehicleInfo == null)
+			{
+				return;
+			}
+			Array16<Vehicle> vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+			if (CreateVehicle(out var vehicle, ref Singleton<SimulationManager>.instance.m_randomizer, vehicleInfo, data.m_position, material, transferToSource: false, transferToTarget: true))
+			{
+				vehicleInfo.m_vehicleAI.SetSource(vehicle, ref vehicles.m_buffer[vehicle], buildingID);
+				VehicleAIStartTransfer(vehicle, ref vehicles.m_buffer[vehicle], offer);
+			}
+		}
+
+		public static void VehicleAIStartTransfer(ushort vehicleID, ref Vehicle data, ExtendedTransferManager.Offer offer)
+		{
+			if ((data.m_flags & Vehicle.Flags.WaitingTarget) != 0)
+			{
+				data.Info.m_vehicleAI.SetTarget(vehicleID, ref data, offer.Building);
+			}
+		}
+
+		public static VehicleInfo GetSelectedVehicle(ushort buildingId)
+		{
+			if (!Singleton<BuildingManager>.instance.TryGetSelectedServiceBuildingVehicle(buildingId, out var prefabIndex))
+			{
+				return null;
+			}
+			return PrefabCollection<VehicleInfo>.GetPrefab((uint)prefabIndex);
+		}
+
+		public static bool CreateVehicle(out ushort vehicle, ref Randomizer r, VehicleInfo info, Vector3 position, ExtendedTransferManager.TransferReason type, bool transferToSource, bool transferToTarget)
+		{
+			Array16<Vehicle> m_vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+			VehicleManager instance2 = Singleton<VehicleManager>.instance;
+			if (m_vehicles.CreateItem(out var item, ref r))
+			{
+				vehicle = item;
+				Vehicle.Frame frame = new Vehicle.Frame(position, Quaternion.identity);
+				m_vehicles.m_buffer[vehicle].m_flags = Vehicle.Flags.Created;
+				m_vehicles.m_buffer[vehicle].m_flags2 = (Vehicle.Flags2)0;
+				if (transferToSource)
+				{
+					m_vehicles.m_buffer[vehicle].m_flags |= Vehicle.Flags.TransferToSource;
+				}
+				if (transferToTarget)
+				{
+					m_vehicles.m_buffer[vehicle].m_flags |= Vehicle.Flags.TransferToTarget;
+				}
+				m_vehicles.m_buffer[vehicle].Info = info;
+				m_vehicles.m_buffer[vehicle].m_frame0 = frame;
+				m_vehicles.m_buffer[vehicle].m_frame1 = frame;
+				m_vehicles.m_buffer[vehicle].m_frame2 = frame;
+				m_vehicles.m_buffer[vehicle].m_frame3 = frame;
+				m_vehicles.m_buffer[vehicle].m_targetPos0 = Vector4.zero;
+				m_vehicles.m_buffer[vehicle].m_targetPos1 = Vector4.zero;
+				m_vehicles.m_buffer[vehicle].m_targetPos2 = Vector4.zero;
+				m_vehicles.m_buffer[vehicle].m_targetPos3 = Vector4.zero;
+				m_vehicles.m_buffer[vehicle].m_sourceBuilding = 0;
+				m_vehicles.m_buffer[vehicle].m_targetBuilding = 0;
+				m_vehicles.m_buffer[vehicle].m_transferType = (byte)type;
+				m_vehicles.m_buffer[vehicle].m_transferSize = 0;
+				m_vehicles.m_buffer[vehicle].m_waitCounter = 0;
+				m_vehicles.m_buffer[vehicle].m_blockCounter = 0;
+				m_vehicles.m_buffer[vehicle].m_nextGridVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_nextOwnVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_nextGuestVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_nextLineVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_transportLine = 0;
+				m_vehicles.m_buffer[vehicle].m_leadingVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_trailingVehicle = 0;
+				m_vehicles.m_buffer[vehicle].m_cargoParent = 0;
+				m_vehicles.m_buffer[vehicle].m_firstCargo = 0;
+				m_vehicles.m_buffer[vehicle].m_nextCargo = 0;
+				m_vehicles.m_buffer[vehicle].m_citizenUnits = 0u;
+				m_vehicles.m_buffer[vehicle].m_path = 0u;
+				m_vehicles.m_buffer[vehicle].m_lastFrame = 0;
+				m_vehicles.m_buffer[vehicle].m_pathPositionIndex = 0;
+				m_vehicles.m_buffer[vehicle].m_lastPathOffset = 0;
+				m_vehicles.m_buffer[vehicle].m_gateIndex = 0;
+				m_vehicles.m_buffer[vehicle].m_waterSource = 0;
+				m_vehicles.m_buffer[vehicle].m_touristCount = 0;
+				m_vehicles.m_buffer[vehicle].m_custom = 0;
+				info.m_vehicleAI.CreateVehicle(vehicle, ref m_vehicles.m_buffer[vehicle]);
+				info.m_vehicleAI.FrameDataUpdated(vehicle, ref m_vehicles.m_buffer[vehicle], ref m_vehicles.m_buffer[vehicle].m_frame0);
+				instance2.m_vehicleCount = (int)(m_vehicles.ItemCount() - 1);
+				return true;
+			}
+			vehicle = 0;
+			return false;
+		}
+	}
+}
---
+++ Code/ExtendedTransferManager.cs
@@ -0,0 +1,301 @@
+﻿using ColossalFramework;
+using System;
+using UnityEngine;
+
+namespace MoreTransferReasons.Code
+{
+	public class ExtendedTransferManager : SimulationManagerBase<TransferManager, TransferProperties>, ISimulationManager
+	{
+		public struct Offer
+		{
+			public byte m_isLocalPark;
+			public Vector3 Position;
+			public int Amount;
+			public bool Active;
+			public ushort Building;
+			public ushort Vehicle;
+			public ushort Citizen;
+		}
+
+		public enum TransferReason
+		{
+			DeliveryLow = 0, // 0 - 255
+			DeliveryNormal = 1, // 256 - 511
+			DeliveryHigh = 2, // 512 - 767
+			FoodSupplies = 3, // 768 - 1023
+			DrinkSupplies = 4, // 1024 - 1279
+			Bread = 5, // 1280 - 1535
+			None = 128
+		}
+
+		int[] IncomingIndexes;
+
+		int[] OutgoingIndexes;
+
+		Offer[] OutgoingOffers;
+
+		Offer[] IncomingOffers;
+
+		int Transfers_Length;
+
+		protected override void Awake()
+		{
+			base.Awake();
+			Transfers_Length = Enum.GetNames(typeof(TransferReason)).Length - 1;
+			OutgoingIndexes = new int[Transfers_Length];
+			IncomingIndexes = new int[Transfers_Length];
+			OutgoingOffers = new Offer[Transfers_Length*256];
+			IncomingOffers = new Offer[Transfers_Length*256];
+		}
+
+		public static TransferReason GetFrameReason(int frameIndex)
+		{
+			return frameIndex switch
+			{
+				1 => TransferReason.DeliveryLow, 
+				3 => TransferReason.DeliveryNormal, 
+				5 => TransferReason.DeliveryHigh, 
+				7 => TransferReason.FoodSupplies, 
+				9 => TransferReason.DrinkSupplies, 
+				_ => TransferReason.None, 
+			};
+		}
+
+		public void AddOutgoingOffer(TransferReason material, Offer offer)
+		{
+			int index = OutgoingIndexes[(int)material];
+			if(index < 256)
+			{
+				OutgoingIndexes[(int)material] = index + 1;
+				int FreeIndex = (int)material * 256 + index;
+				OutgoingOffers[FreeIndex] = offer;
+			}
+		}
+
+		public void AddIncomingOffer(TransferReason material, Offer offer)
+		{
+			int index = IncomingIndexes[(int)material];
+			if(index < 256)
+			{
+				IncomingIndexes[(int)material] = index + 1;
+				int FreeIndex = (int)material * 256 + index;
+				IncomingOffers[FreeIndex] = offer;
+			}
+		}
+
+		protected override void SimulationStepImpl(int subStep)
+		{
+			if (subStep != 0)
+			{
+				int frameIndex = (int)(Singleton<SimulationManager>.instance.m_currentFrameIndex & 0xFF);
+				MatchOffers(GetFrameReason(frameIndex));
+			}
+		}
+
+		private void MatchOffers(TransferReason material)
+		{
+			if (material == TransferReason.None)
+			{
+				return;
+			}
+			int outgoing_matched_count = 0;
+			int incoming_matched_count = 0;
+			int outgoing_ocuppied_count = OutgoingIndexes[(int)material];
+			int incoming_ocuppied_count = IncomingIndexes[(int)material];
+			while(outgoing_matched_count < outgoing_ocuppied_count || incoming_matched_count < incoming_ocuppied_count)
+			{
+				if(outgoing_matched_count < outgoing_ocuppied_count)
+				{
+					Offer outgoing_offer = OutgoingOffers[(int)material * 256 + outgoing_matched_count];
+					Vector3 outgoing_position = outgoing_offer.Position;
+					int outgoing_amount = outgoing_offer.Amount;
+					while(outgoing_amount != 0)
+					{
+						int chosen_index = 0;
+						float min_distance = -1f;
+						for(int i = 0; i < incoming_ocuppied_count; i++)
+						{
+							Offer incoming_offer = IncomingOffers[(int)material * 256 + i];
+							float distance = Vector3.SqrMagnitude(incoming_offer.Position - outgoing_position);
+							if(distance > min_distance)
+							{
+								chosen_index = i;
+								min_distance = distance;
+								if(distance < 0f)
+								{
+									break;
+								}
+							}
+						}
+						Offer chosen_incoming_offer = IncomingOffers[(int)material * 256 + chosen_index];
+						int incoming_amount = chosen_incoming_offer.Amount;
+						int min_amount = Mathf.Min(outgoing_amount, incoming_amount);
+						if (min_amount != 0)
+						{
+							StartTransfer(material, chosen_incoming_offer, outgoing_offer, min_amount);
+						}
+						outgoing_amount -= min_amount;
+						incoming_amount -= min_amount;
+						if (incoming_amount == 0)
+						{
+							int new_index = IncomingIndexes[(int)material] - 1;
+							IncomingIndexes[(int)material] = new_index;
+							ref Offer reference = ref IncomingOffers[(int)material * 256 + chosen_index];
+							reference = IncomingOffers[(int)material * 256 + new_index];
+							incoming_ocuppied_count = new_index;
+						}
+						else
+						{
+							chosen_incoming_offer.Amount = incoming_amount;
+							IncomingOffers[(int)material * 256 + chosen_index] = chosen_incoming_offer;
+						}
+						outgoing_offer.Amount = outgoing_amount;
+					}
+					if (outgoing_amount == 0)
+					{
+						outgoing_ocuppied_count--;
+						OutgoingIndexes[(int)material] = outgoing_ocuppied_count;
+						ref Offer reference2 = ref OutgoingOffers[(int)material * 256 + outgoing_matched_count];
+						reference2 = OutgoingOffers[(int)material * 256 + outgoing_ocuppied_count];
+					}
+					else
+					{
+						outgoing_offer.Amount = outgoing_amount;
+						OutgoingOffers[(int)material * 256 + outgoing_matched_count] = outgoing_offer;
+						outgoing_matched_count++;
+					}
+				}
+				if(incoming_matched_count < incoming_ocuppied_count)
+				{
+					Offer incoming_offer = IncomingOffers[(int)material * 256 + outgoing_matched_count];
+					Vector3 incoming_position = incoming_offer.Position;
+					int incoming_amount = incoming_offer.Amount;
+					while(incoming_amount != 0)
+					{
+						int chosen_index = 0;
+						float min_distance = -1f;
+						for(int i = 0; i < outgoing_ocuppied_count; i++)
+						{
+							Offer outgoing_offer = OutgoingOffers[(int)material * 256 + i];
+							float distance = Vector3.SqrMagnitude(outgoing_offer.Position - incoming_position);
+							if(distance > min_distance)
+							{
+								chosen_index = i;
+								min_distance = distance;
+								if(distance < 0f)
+								{
+									break;
+								}
+							}
+						}
+						Offer chosen_outgoing_offer = OutgoingOffers[(int)material * 256 + chosen_index];
+						int outgoing_amount = chosen_outgoing_offer.Amount;
+						int min_amount = Mathf.Min(outgoing_amount, incoming_amount);
+						if (min_amount != 0)
+						{
+							StartTransfer(material, chosen_outgoing_offer, incoming_offer, min_amount);
+						}
+						incoming_amount -= min_amount;
+						outgoing_amount -= min_amount;
+						if (outgoing_amount == 0)
+						{
+							int new_index = OutgoingIndexes[(int)material] - 1;
+							OutgoingIndexes[(int)material] = new_index;
+							ref Offer reference = ref OutgoingOffers[(int)material * 256 + chosen_index];
+							reference = OutgoingOffers[(int)material * 256 + new_index];
+							outgoing_ocuppied_count = new_index;
+						}
+						else
+						{
+							chosen_outgoing_offer.Amount = outgoing_amount;
+							OutgoingOffers[(int)material * 256 + chosen_index] = chosen_outgoing_offer;
+						}
+						incoming_offer.Amount = incoming_amount;
+					}
+					if (incoming_amount == 0)
+					{
+						incoming_ocuppied_count--;
+						IncomingIndexes[(int)material] = incoming_ocuppied_count;
+						ref Offer reference2 = ref IncomingOffers[(int)material * 256 + incoming_matched_count];
+						reference2 = IncomingOffers[(int)material * 256 + incoming_ocuppied_count];
+					}
+					else
+					{
+						incoming_offer.Amount = incoming_amount;
+						IncomingOffers[(int)material * 256 + incoming_matched_count] = incoming_offer;
+						incoming_matched_count++;
+					}
+				}
+			}
+			for(int k = 0; k < Transfers_Length; k++)
+			{
+				OutgoingIndexes[k] = 0;
+				IncomingIndexes[k] = 0;
+			}
+		}
+
+		private void StartTransfer(TransferReason material, Offer offerOut, Offer offerIn, int delta)
+		{
+			bool active = offerIn.Active;
+			bool active2 = offerOut.Active;
+			if (active && offerIn.Vehicle != 0)
+			{
+				Array16<Vehicle> vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+				ushort vehicle = offerIn.Vehicle;
+				CustomStartTransfer.VehicleAIStartTransfer(vehicle, ref vehicles.m_buffer[vehicle], offerOut);
+				offerOut.Amount = delta;
+			}
+			else if (active2 && offerOut.Vehicle != 0)
+			{
+				Array16<Vehicle> vehicles2 = Singleton<VehicleManager>.instance.m_vehicles;
+				ushort vehicle2 = offerOut.Vehicle;
+				CustomStartTransfer.VehicleAIStartTransfer(vehicle2, ref vehicles2.m_buffer[vehicle2], offerIn);
+				offerIn.Amount = delta;
+			}
+			else if (active2 && offerOut.Building != 0)
+			{
+				Array16<Building> buildings = Singleton<BuildingManager>.instance.m_buildings;
+				if (offerOut.m_isLocalPark != 0 && offerOut.m_isLocalPark == offerIn.m_isLocalPark)
+				{
+					StartDistrictTransfer(material, offerOut, offerIn);
+					return;
+				}
+				ushort building = offerOut.Building;
+				CustomStartTransfer.BuildingAIStartTransfer(building, ref buildings.m_buffer[building], material, offerIn);
+				offerIn.Amount = delta;
+			}
+			else if (active && offerIn.Building != 0)
+			{
+				if (offerIn.m_isLocalPark != 0 && offerIn.m_isLocalPark == offerOut.m_isLocalPark)
+				{
+					StartDistrictTransfer(material, offerOut, offerIn);
+					return;
+				}
+				Array16<Building> buildings2 = Singleton<BuildingManager>.instance.m_buildings;
+				ushort building2 = offerIn.Building;
+				CustomStartTransfer.BuildingAIStartTransfer(building2, ref buildings2.m_buffer[building2], material, offerOut);
+				offerOut.Amount = delta;
+			}
+		}
+
+		private void StartDistrictTransfer(TransferReason material, Offer offerOut, Offer offerIn)
+		{
+			Array16<Building> buildings = Singleton<BuildingManager>.instance.m_buildings;
+			ushort building = offerOut.Building;
+			ushort building2 = offerIn.Building;
+			BuildingInfo info = buildings.m_buffer[building].Info;
+			BuildingInfo info2 = buildings.m_buffer[building2].Info;
+			info.m_buildingAI.GetMaterialAmount(building, ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[building], material, out var amount, out var _);
+			info2.m_buildingAI.GetMaterialAmount(building2, ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[building2], material, out var amount2, out var max2);
+			int num = Math.Min(amount, max2 - amount2);
+			if (num > 0)
+			{
+				amount = -num;
+				amount2 = num;
+				info.m_buildingAI.ModifyMaterialBuffer(building, ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[building], material, ref amount);
+				info2.m_buildingAI.ModifyMaterialBuffer(building2, ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[building2], material, ref amount2);
+			}
+		}
+
+	}
+}
