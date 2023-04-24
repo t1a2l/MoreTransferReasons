171193c5bbf969b73b780737a991c0df1bd815e2
Tal <talrofeh@gmail.com>
2023-04-25 01:48:45 +0300

    enable manager to start working

--- MoreTransferReasonsMod.cs
+++ MoreTransferReasonsMod.cs
@@ -1,6 +1,7 @@
 ﻿using ICities;
 using CitiesHarmony.API;
 using MoreTransferReasons.Utils;
+using UnityEngine;
 
 namespace MoreTransferReasons
 {
@@ -19,6 +20,29 @@ namespace MoreTransferReasons
         {
             if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
         }
+
+        public class CustomLoader : LoadingExtensionBase
+        {
+            /// <summary>
+            /// This event is triggerred when a level is loaded
+            /// </summary>
+            public override void OnLevelLoaded(LoadMode mode)
+            {
+                // Instantiate a custom object
+                GameObject go = new GameObject("ExtendedTransferManagerMod");
+                go.AddComponent<ExtendedTransferManagerComponent>();
+
+                base.OnLevelLoaded(mode);
+            }
+        }
+
+        public class ExtendedTransferManagerComponent : MonoBehaviour
+		{
+            void Start()
+            {
+                SimulationManager.RegisterManager(ColossalFramework.Singleton<ExtendedTransferManager>.instance);
+            }
+		}
 
 
     }
