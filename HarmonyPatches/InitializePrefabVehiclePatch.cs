7955137429cad1da5f41cf02a5befaa19c30276d
Tal <talrofeh@gmail.com>
2025-03-13 20:20:10 +0200

    steel vehicle to move steel

--- HarmonyPatches/InitializePrefabVehiclePatch.cs
+++ HarmonyPatches/InitializePrefabVehiclePatch.cs
@@ -19,6 +19,10 @@ namespace MoreTransferReasons.HarmonyPatches
                     UnityEngine.Object.DestroyImmediate(oldAI);
                     var newAI = (PrefabAI)__instance.gameObject.AddComponent<ExtendedCargoTruckAI>();
                     PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    if(__instance.name.Contains("Girder"))
+                    {
+                        __instance.m_class.m_level = ItemClass.Level.Level3;
+                    }
                 }
                 if(oldAI is PassengerCarAI && !__instance.name.Contains("Trailer") && Utils.Settings.ExtendedPassengerCarAI.value == true)
                 {
