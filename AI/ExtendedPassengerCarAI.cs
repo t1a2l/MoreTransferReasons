39a401867c9b602b5d39b938313a42804838e66e
Tal <talrofeh@gmail.com>
2025-03-16 00:23:20 +0200

    remove not needed check

--- AI/ExtendedPassengerCarAI.cs
+++ AI/ExtendedPassengerCarAI.cs
@@ -4,18 +4,6 @@
     {
         void IExtendedVehicleAI.ExtendedStartTransfer(ushort vehicleID, ref Vehicle data, ExtendedTransferManager.TransferReason material, ExtendedTransferManager.Offer offer)
         {
-            var transferType = data.m_transferType;
-            if (data.m_transferType >= 200)
-            {
-                transferType = (byte)(data.m_transferType - 200);
-            }
-            if (material == (ExtendedTransferManager.TransferReason)transferType)
-            {
-                if ((data.m_flags & Vehicle.Flags.WaitingTarget) != 0)
-                {
-                    SetTarget(vehicleID, ref data, offer.Building);
-                }
-            }
             if (material == ExtendedTransferManager.TransferReason.FuelVehicle)
             {
                 data.m_custom = (ushort)material;
