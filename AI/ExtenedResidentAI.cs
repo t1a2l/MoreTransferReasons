cd4705f3be9e3e55b300e62c453913834c54524b
Tal <talrofeh@gmail.com>
2025-04-17 16:20:08 +0300

    add car sell, check correct source building

--- AI/ExtenedResidentAI.cs
+++ AI/ExtenedResidentAI.cs
@@ -8,22 +8,38 @@
             {
                 return;
             }
+            ushort source_building = 0;
+            switch(data.CurrentLocation)
+            {
+                case Citizen.Location.Home:
+                    source_building = data.m_homeBuilding; 
+                    break;
+
+                case Citizen.Location.Work:
+                    source_building = data.m_workBuilding;
+                    break;
+
+                case Citizen.Location.Visit:
+                    source_building = data.m_visitBuilding;
+                    break;
+            }
             switch (material)
             {
                 case ExtendedTransferManager.TransferReason.MealsLow:
                 case ExtendedTransferManager.TransferReason.MealsMedium:
                 case ExtendedTransferManager.TransferReason.MealsHigh:
                     data.m_flags &= ~Citizen.Flags.Evacuating;
-                    if (StartMoving(citizenID, ref data, data.m_visitBuilding, offer.Building))
+                    if (StartMoving(citizenID, ref data, source_building, offer.Building))
                     {
                         data.SetVisitplace(citizenID, offer.Building, 0u);
                     }
                     break;
                 case ExtendedTransferManager.TransferReason.CarBuy:
+                case ExtendedTransferManager.TransferReason.CarSell:
                 case ExtendedTransferManager.TransferReason.FuelVehicle:
                 case ExtendedTransferManager.TransferReason.FuelElectricVehicle:
                     data.m_flags &= ~Citizen.Flags.Evacuating;
-                    StartMoving(citizenID, ref data, data.m_visitBuilding, offer.Building);
+                    StartMoving(citizenID, ref data, source_building, offer.Building);
                     break;
             }
         }
--- AI/ExtenedTouristAI.cs
+++ AI/ExtenedTouristAI.cs
@@ -8,13 +8,28 @@
             {
                 return;
             }
+            ushort source_building = 0;
+            switch (data.CurrentLocation)
+            {
+                case Citizen.Location.Home:
+                    source_building = data.m_homeBuilding;
+                    break;
+
+                case Citizen.Location.Work:
+                    source_building = data.m_workBuilding;
+                    break;
+
+                case Citizen.Location.Visit:
+                    source_building = data.m_visitBuilding;
+                    break;
+            }
             switch (material)
             {
                 case ExtendedTransferManager.TransferReason.MealsLow:
                 case ExtendedTransferManager.TransferReason.MealsMedium:
                 case ExtendedTransferManager.TransferReason.MealsHigh:
                     data.m_flags &= ~Citizen.Flags.Evacuating;
-                    if (StartMoving(citizenID, ref data, data.m_visitBuilding, offer.Building))
+                    if (StartMoving(citizenID, ref data, source_building, offer.Building))
                     {
                         data.SetVisitplace(citizenID, offer.Building, 0u);
                     }
@@ -23,7 +38,7 @@
                 case ExtendedTransferManager.TransferReason.FuelVehicle:
                 case ExtendedTransferManager.TransferReason.FuelElectricVehicle:
                     data.m_flags &= ~Citizen.Flags.Evacuating;
-                    StartMoving(citizenID, ref data, data.m_visitBuilding, offer.Building);
+                    StartMoving(citizenID, ref data, source_building, offer.Building);
                     break;
             }
         }
