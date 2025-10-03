e07f3f3b3eec3cc626e27f62c14727f83cca461d
Tal <talrofeh@gmail.com>
2025-10-03 23:18:18 +0300

    support cars and house parts vehicle trailer not showing

---
+++ HarmonyPatches/VehicleManagerPatch.cs
@@ -0,0 +1,28 @@
+﻿using ColossalFramework.Math;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MoreTransferReasons.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class VehicleManagerPatch
+    {
+        [HarmonyPatch(typeof(VehicleManager), "CreateVehicle")]
+        [HarmonyPostfix]
+        public static void CreateVehicle(VehicleManager __instance, ref ushort vehicle, ref Randomizer r, VehicleInfo info, Vector3 position, TransferManager.TransferReason type, bool transferToSource, bool transferToTarget, ref bool __result)
+        {
+            if(__result && (type == ExtendedTransferManager.Cars || type == ExtendedTransferManager.HouseParts))
+            {
+                if (type == ExtendedTransferManager.Cars)
+                {
+                    __instance.m_vehicles.m_buffer[vehicle].m_gateIndex = 1;
+                }
+                else if (type == ExtendedTransferManager.HouseParts)
+                {
+                    __instance.m_vehicles.m_buffer[vehicle].m_gateIndex = 9;
+                }
+            }
+        }
+
+    }
+}
